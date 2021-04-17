9c122408c741c6fa061ecf62929f9509865f6a55
Chris Nisbet <nischris@gmail.com>
2021-04-17 13:00:07 +1200

    Restored wwwmoint point to original value
    Not sure why the change was needed in the first place, but it doesn't
    appear to be needed now, so restoring it.

--- src/main.c
+++ src/main.c
@@ -337,7 +337,7 @@ int main(int argc, char *argv[])
 	static struct lws_http_mount wwwmount = {
 		NULL,
 		"/",
-		"/www",   // anything not-a-dir is ok, so our HTTP code runs and not lws
+		"/dev/null",   // anything not-a-dir is ok, so our HTTP code runs and not lws
 		"index.html"
 	};
 	wwwmount.cache_reusable = !!www_maxage;
